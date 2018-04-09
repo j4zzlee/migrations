9f8904fe5ef9f4bc2cc18daf760fdad14959f46e
Le Huu Hoang Gia <hoanggia.lh@gmail.com>
2018-04-10 00:41:17 +0700

    Centralize repositories

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # migrations
+
+## Build & push to nuget
+```zsh
+dotnet pack .\st2forget.migrations\
+dotnet nuget push AppLogger.1.0.0.nupkg -k qz2jga8pl3dvn2akksyquwcs9ygggg4exypy3bhxy6w6x6 -s https://api.nuget.org/v3/index.json
+```
---
+++ st2forget.commons.datetime/DateTimeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace st2forget.commons.datetime
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// To the unix timestamp.
+        /// </summary>
+        /// <param name="dateTime">The date time.</param>
+        /// <returns></returns>
+        public static long ToUnixTimestamp(this DateTime dateTime)
+        {
+            return DateTimeOffset.Now.ToUnixTimeSeconds();
+        }
+
+        /// <summary>
+        /// Unixes the time to time.
+        /// </summary>
+        /// <param name="timeStamp">The time stamp.</param>
+        /// <returns></returns>
+        public static DateTime UnixTimeToTime(this long timeStamp)
+        {
+            return DateTimeOffset.FromUnixTimeSeconds(timeStamp).UtcDateTime;
+        }
+    }
+}
